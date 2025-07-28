9bcc50f726ac31a473f7c99d718d9206c12529da
KhairulFilhan <khairulfilhan94@gmail.com>
2025-07-29 01:02:58 +0800

    Add ToastBar component for displaying toast notifications and update package dependencies

---
+++ components/ui/ToastBar.tsx
@@ -0,0 +1,14 @@
+import Toast from 'react-native-toast-message';
+
+export const showToast = ({ type = 'success', text1 = '', text2 = '' }) => {
+  Toast.show({
+    type,
+    text1,
+    text2,
+    position: 'top',
+    visibilityTime: 3000,
+    autoHide: true,
+  });
+};
+
+export const ToastBar = Toast;
