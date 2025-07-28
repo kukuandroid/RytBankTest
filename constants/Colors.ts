315a48ca5087d1b2491c69d7e914bf0767d7dcca
KhairulFilhan <khairulfilhan94@gmail.com>
2025-07-28 23:29:12 +0800

    Refactor color constants in Colors.ts for improved clarity and consistency; update background colors for light and dark modes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,26 +3,29 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0101e5';
+const primaryColor = '#0101e5';
+const tintColorLight = '#0F172A';
 const subColor = '#5050acff';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#0101e5',
+    background: '#0F172A',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
     subColor: subColor,
+    primaryColor: primaryColor,
   },
   dark: {
     text: '#ECEDEE',
-    background: '#0101e5',
+    background: '#0F172A',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     subColor: subColor,
+    primaryColor: primaryColor,
   },
 };
